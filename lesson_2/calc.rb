3de9889660ead1e1ed9b3f6ae366558e04c86d2a
Eric Londer <ericlonder730@gmail.com>
2021-05-08 17:15:19 -0400

    get num1 from  user

--- lesson_2/calc.rb
+++ lesson_2/calc.rb
@@ -3,5 +3,12 @@
 # perform the operation on the two numnbers
 # output the result
 
-answer = Kernel.gets()
-Kernel.puts() answer
+# answer = Kernel.gets()
+# Kernel.puts(answer)
+
+Kernel.puts("Welcome to Calculator")
+
+Kernel.puts("What's the first number?")
+num1 = Kernel.gets()
+
+Kernel.puts("The number is " num1 + "!")
