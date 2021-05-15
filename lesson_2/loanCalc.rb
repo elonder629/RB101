5f8bf87b09b3c45331bfae199677386cff4370a3
Eric Londer <ericlonder730@gmail.com>
2021-05-15 01:21:52 -0400

    Rubocop offense fixes

--- lesson_2/loanCalc.rb
+++ lesson_2/loanCalc.rb
@@ -2,25 +2,25 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-def is_float?(input)
+def float?(input)
   /\d/.match(input) && /^-?\d*\.?\d*$/.match(input)
 end
 
-def is_int?(input)
+def int?(input)
   /^-?\d+$/.match(input)
 end
 
-def is_valid?(input)
-  if is_float?(input) && input.to_f > 0
-    return input.to_f
-  elsif is_int?(input) && input.to_i > 0
-    return input.to_i
+def valid?(input)
+  if float?(input) && input.to_f > 0
+    input.to_f
+  elsif int?(input) && input.to_i > 0
+    input.to_i
   else
-    return false
+    false
   end
 end
 
-def loan_formula(p,n,j)
+def loan_formula(p, n, j)
   (p * (j / (1 - (1 + j)**(-n)))).round(2)
 end
 
@@ -41,7 +41,7 @@ loop do # main loop
   loan_ammount = nil
   loop do
     prompt("What is the total loan ammount?")
-    loan_ammount = is_valid?(gets.chomp)
+    loan_ammount = valid?(gets.chomp)
     if loan_ammount
       break
     else
@@ -51,7 +51,7 @@ loop do # main loop
   loan_duration = nil
   loop do
     prompt("What is the total loan duration in months")
-    loan_duration = is_valid?(gets.chomp)
+    loan_duration = valid?(gets.chomp)
     if loan_duration
       break
     else
@@ -61,15 +61,17 @@ loop do # main loop
   apr = nil
   loop do
     prompt("What is the APR as a percentage")
-    apr = is_valid?(gets.chomp)
+    apr = valid?(gets.chomp)
     if apr
       break
     else
       prompt("Please enter a valid number")
     end
   end
-  monthly_interest_rate = (apr/100)/12
-  monthly_payment = loan_formula(loan_ammount,loan_duration,monthly_interest_rate)
+  monthly_interest_rate = (apr / 100) / 12
+  monthly_payment = loan_formula(loan_ammount,
+                                 loan_duration,
+                                 monthly_interest_rate)
 
   prompt("Your monthy monthly payment will be #{monthly_payment} Dollars")
   prompt("Would you like to make another Calculation? Enter Y")
